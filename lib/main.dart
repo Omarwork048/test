136239e98c326f0c31f86cd92a53ca5ae9fc0254
Omar-ELbakh <139764743+Omarwork048@users.noreply.github.com>
2023-07-18 18:08:47 +0300

    create loginScreen and constants

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:test/screens/login_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,21 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const HomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container();
+        initialRoute: LoginScreen.id,
+        routes: {LoginScreen.id: (context) => LoginScreen()});
   }
 }
---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:test/screens/constants.dart';
+
+class LoginScreen extends StatelessWidget {
+  static String id = 'LiginScreen';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: KmainColor,
+      body: Container(),
+    );
+  }
+}
