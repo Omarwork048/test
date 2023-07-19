612de1281e939799ac39373e056b1731e13a6605
Omar-ELbakh <139764743+Omarwork048@users.noreply.github.com>
2023-07-19 10:08:54 +0300

    Update signup_screen.dart

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -8,7 +8,7 @@ class SignUp extends StatefulWidget {
   _SignUpState createState() => _SignUpState();
 }
 
-class _SignUpState extends State<SignUp> with RegisterAuth {
+class _SignUpState extends State<SignUp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,7 +44,7 @@ class _SignUpState extends State<SignUp> with RegisterAuth {
                   ),
                 ),
                 Form(
-                  key: formKey,
+                  // key: formKey,
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
@@ -54,38 +54,22 @@ class _SignUpState extends State<SignUp> with RegisterAuth {
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: <Widget>[
                             const SizedBox(height: 25.0),
-                            CustomTextField(),
-                            TextFormField(
-                              decoration: buildInputDecoration("Email"),
-                              validator: InputValidator.email,
+                            CustomTextField(
+                              hintText: "Username or Phone Number",
                             ),
                             const SizedBox(height: 8.0),
                             Padding(
-                              padding:
-                                  const EdgeInsets.symmetric(vertical: 8.0),
-                              child: TextFormField(
-                                obscureText: visiblePassword,
-                                decoration:
-                                    buildInputDecoration("Password", true),
-                                validator: InputValidator.password,
-                                onChanged: (t) {
-                                  InputValidator.passwordText = t;
-                                },
-                              ),
-                            ),
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 8.0),
+                                child: CustomTextField(
+                                  hintText: "Create Password",
+                                )),
                             Padding(
-                              padding:
-                                  const EdgeInsets.symmetric(vertical: 8.0),
-                              child: TextFormField(
-                                obscureText: visiblePassword,
-                                onChanged: (t) {
-                                  InputValidator.confirmPasswordText = t;
-                                },
-                                decoration: buildInputDecoration(
-                                    "Re-enter password", true),
-                                validator: InputValidator.password,
-                              ),
-                            ),
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 8.0),
+                                child: CustomTextField(
+                                  hintText: "Confirm Password",
+                                )),
                           ],
                         ),
                       ),
@@ -98,18 +82,9 @@ class _SignUpState extends State<SignUp> with RegisterAuth {
                           height: 40.0,
                           minWidth: MediaQuery.of(context).size.width,
                           color: KmainColor,
-                          onPressed: () {
-                            register();
-                          },
-                          child: isLoading
-                              ? const SizedBox(
-                                  width: 20.0,
-                                  height: 20.0,
-                                  child: CircularProgressIndicator(
-                                    strokeWidth: 2.0,
-                                  ))
-                              : const Text("Create account",
-                                  style: TextStyle(color: Colors.white)),
+                          onPressed: () {},
+                          child: Text("Sign Up",
+                              style: TextStyle(color: Colors.white)),
                         ),
                       ),
                       Container(
@@ -142,84 +117,84 @@ class _SignUpState extends State<SignUp> with RegisterAuth {
     );
   }
 
-  InputDecoration buildInputDecoration(hintText, [eyeVisible = false]) {
-    var outlineInputBorder = OutlineInputBorder(
-      borderSide: BorderSide(color: Colors.grey.withOpacity(0.2)),
-    );
+  // InputDecoration buildInputDecoration(hintText, [eyeVisible = false]) {
+  //   var outlineInputBorder = OutlineInputBorder(
+  //     borderSide: BorderSide(color: Colors.grey.withOpacity(0.2)),
+  //   );
 
-    return InputDecoration(
-      suffixIcon: eyeVisible
-          ? IconButton(
-              icon: Icon(
-                Icons.remove_red_eye,
-                color: !visiblePassword ? Colors.grey : Colors.red,
-              ),
-              onPressed: () {
-                setState(() {
-                  visiblePassword = !visiblePassword;
-                });
-              },
-            )
-          : const SizedBox.shrink(),
-      hintText: hintText,
-      contentPadding: const EdgeInsets.symmetric(horizontal: 16.0),
-      border: outlineInputBorder,
-      enabledBorder: outlineInputBorder,
-      focusedBorder: outlineInputBorder,
-    );
-  }
-
-  @override
-  Future register() async {
-    if (formKey.currentState!.validate()) {
-      formKey.currentState!.save();
-      setState(() {
-        isLoading = true;
-      });
-      if (rememberMe) {
-        debugPrint("saved");
-      }
-      await Future.delayed(const Duration(seconds: 1));
-      setState(() {
-        isLoading = false;
-      });
-    }
-    return null;
-  }
-}
-
-abstract mixin class RegisterAuth {
-  GlobalKey<FormState> formKey = GlobalKey();
-  bool rememberMe = false;
-  bool visiblePassword = false;
-  bool isLoading = false;
-  Future register();
-}
-
-class InputValidator {
-  static String? confirmPasswordText;
-  static String? passwordText;
-  static String? emailText;
-  static String? name;
-
-  // static emptyCheck(String msg) {
-  //   return (t) {
-  //     if (t.isEmpty || t == null) return msg;
-  //     return null;
-  //   };
+  //   return InputDecoration(
+  //     suffixIcon: eyeVisible
+  //          IconButton(
+  //             icon: Icon(
+  //               Icons.remove_red_eye,
+  //               color: !visiblePassword ? Colors.grey : Colors.red,
+  //             ),
+  //             onPressed: () {
+  //               setState(() {
+  //                 visiblePassword = !visiblePassword;
+  //               });
+  //             },
+  //           )
+  //         : const SizedBox.shrink(),
+  //     hintText: hintText,
+  //     contentPadding: const EdgeInsets.symmetric(horizontal: 16.0),
+  //     border: outlineInputBorder,
+  //     enabledBorder: outlineInputBorder,
+  //     focusedBorder: outlineInputBorder,
+  //   );
   // }
 
-  static String? email(String? t) {
-    if (t!.isEmpty) return "Email can't be empty";
-    return null;
-  }
-
-  static String? password(String? t) {
-    if (t!.isEmpty) return "Password can't be empty";
-    if (confirmPasswordText != passwordText) return "Password not matched";
-    return null;
-  }
+  // @override
+  // Future register() async {
+  //   if (formKey.currentState!.validate()) {
+  //     formKey.currentState!.save();
+  //     setState(() {
+  //       isLoading = true;
+  //     });
+  //     if (rememberMe) {
+  //       debugPrint("saved");
+  //     }
+  //     await Future.delayed(const Duration(seconds: 1));
+  //     setState(() {
+  //       isLoading = false;
+  //     });
+  //   }
+  //   return null;
+  // }
 }
+
+// abstract mixin class RegisterAuth {
+//   GlobalKey<FormState> formKey = GlobalKey();
+//   bool rememberMe = false;
+//   bool visiblePassword = false;
+//   bool isLoading = false;
+//   Future register();
+// }
+
+// class InputValidator {
+//   static String? confirmPasswordText;
+//   static String? passwordText;
+//   static String? emailText;
+//   static String? name;
+
+//   // static emptyCheck(String msg) {
+//   //   return (t) {
+//   //     if (t.isEmpty || t == null) return msg;
+//   //     return null;
+//   //   };
+//   // }
+
+//   static String? email(String? t) {
+//     if (t!.isEmpty) return "Email can't be empty";
+//     return null;
+//   }
+
+//   static String? password(String? t) {
+//     if (t!.isEmpty) return "Password can't be empty";
+//     if (confirmPasswordText != passwordText) return "Password not matched";
+//     return null;
+//   }
+// }
 
 // Padding(
 //   padding: const EdgeInsets.only(right: 26.0),
